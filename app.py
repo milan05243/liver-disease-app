d2cd18c9dec795f42044ce77c3011bdc1ec2bd61
Milan Choudhary <milan.rchoudhary@gmail.com>
2026-03-28 22:39:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import joblib
 st.set_page_config(page_title="Liver Predictor", page_icon="🧬", layout="wide")
 
 model = joblib.load("liver_model.pkl")
-imputer = joblib.load("imputer.pkl")
 
 st.title("🧠 Liver Disease Prediction System")
 st.markdown("### AI-powered health risk assessment tool")
@@ -13,19 +12,19 @@ st.markdown("### AI-powered health risk assessment tool")
 col1, col2 = st.columns(2)
 
 with col1:
-    age = st.number_input("Age", value=45)
-    tb = st.number_input("Total Bilirubin", value=1.0)
-    alkphos = st.number_input("Alkaline Phosphotase", value=200)
-    tp = st.number_input("Total Proteins", value=6.5)
-    alb = st.number_input("Albumin", value=3.0)
+    age = st.number_input("Age", value=35)
+    tb = st.number_input("Total Bilirubin", value=0.8)
+    alkphos = st.number_input("Alkaline Phosphotase", value=180)
+    tp = st.number_input("Total Proteins", value=7.0)
+    alb = st.number_input("Albumin", value=4.0)
 
 with col2:
     gender = st.selectbox("Gender", ["Male", "Female"])
     gender = 1 if gender == "Male" else 0
-    db = st.number_input("Direct Bilirubin", value=0.3)
-    alt = st.number_input("ALT", value=30)
-    ast = st.number_input("AST", value=40)
-    ag_ratio = st.number_input("A/G Ratio", value=1.0)
+    db = st.number_input("Direct Bilirubin", value=0.2)
+    alt = st.number_input("ALT", value=25)
+    ast = st.number_input("AST", value=30)
+    ag_ratio = st.number_input("A/G Ratio", value=1.2)
 
 if st.button("Predict"):
     bilirubin_ratio = db / tb if tb != 0 else 0
@@ -46,8 +45,6 @@ if st.button("Predict"):
         enzyme_ratio
     ]])
 
-    input_data = imputer.transform(input_data)
-
     prob = model.predict_proba(input_data)[0][1]
 
     st.subheader("Prediction Result")
@@ -61,6 +58,7 @@ if st.button("Predict"):
         st.error(f"High Risk\nProbability: {prob:.2f}")
 
     st.metric(label="Risk Probability", value=f"{prob*100:.1f}%")
+
 
 # Warning
 st.warning("⚠️ This is an AI-based prediction tool and not a medical diagnosis. Please consult a doctor.")
